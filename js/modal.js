b9e999e40cf8103fbc812e1ef35ba4056e2ee081
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-08-10 19:06:32 +0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,23 @@
 (() => {
   const refs = {
+    openHeaderModalBtn: document.querySelector("[data-header-modal-open]"),
+    closeHeaderModalBtn: document.querySelector("[data-header-modal-close]"),
+    headerModal: document.querySelector("[data-header-modal]"),
     openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtns: document.querySelectorAll("[data-modal-close]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+  refs.openHeaderModalBtn.addEventListener("click", toggleHeaderModal);
+  refs.closeHeaderModalBtn.addEventListener("click", toggleHeaderModal);
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtns.forEach((btn) =>
-    btn.addEventListener("click", toggleModal)
-  );
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+  }
+
+  function toggleHeaderModal() {
+    refs.headerModal.classList.toggle("is-hidden");
   }
 })();
